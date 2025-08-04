29eb6b08466384ae3e53c969f258359f34d76b97
bahamondex <oscar.bahamonde@oscarbahamonde.awsapps.com>
2025-08-03 21:22:23 -0500

    fc

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.tools.email_tool import EmailTool, EmailResponse
 from src.tools.search_tool import SearchTool
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/email", response_model=EmailResponse)
 def send_email(email: EmailTool):
